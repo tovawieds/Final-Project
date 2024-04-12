0c9532a04f1d815b6beb9c8d5c3a1725da98c6a3
tovawieds <155400443+tovawieds@users.noreply.github.com>
2024-04-12 03:01:13 +0000

    04

--- web.py
+++ web.py
@@ -17,21 +17,21 @@ def scrape(s):
     #     print(state)
     #     for part in state:
     #         print(part.text)
-    line = soup.find_all('tr')
+    # line = soup.find_all('tr')
     # print(soup.get_text())
     # for link in soup.find_all('a'):
     #     print(link.get('href'))
     # print(soup.get_text())
     with open('data.csv', 'w', newline='') as file:  # open csv file
-        field_names = ['Rank', 'State', 'Code', 'Population']  # field name
+        field_names = ['Rank', 'State', 'Code', 'Population']  # field names
         writer = csv.DictWriter(file, fieldnames=field_names)  # open a writer
-        list = []
-        for state in soup.find_all('tr'):  # extract the url's and titles
-            state_info = []
-            for part in state:
-                state_info.append(part.text)
-            list.append({'Rank': state_info[0], 'State': state_info[1], 'Code': state_info[2], 'Population': state_info[3]})
-        print(list)
+        list = []  # list of dictionaries with each state
+        for state in soup.find_all('tr'):  # each state
+            state_info = []  # list of info for each state
+            for part in state:  # for each piece in state
+                state_info.append(part.text)  # add it to the list
+            list.append({'Rank': state_info[0], 'State': state_info[1], 'Code': state_info[2], 'Population': state_info[3]})  # put each state into a dictionary and add it to list of states
+        # print(list)
         writer.writeheader()  # write the header
         for row in list:  # write it over to the csv file
             writer.writerow({'Rank': row['Rank'], 'State': row['State'], 'Code': row['Code'], 'Population': row['Population'],})

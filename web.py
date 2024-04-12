719d98f91f3cb9b6efe3190c35b99b7839313a68
tovawieds <155400443+tovawieds@users.noreply.github.com>
2024-04-12 02:12:06 +0000

    03

--- web.py
+++ web.py
@@ -12,17 +12,26 @@ def main():
 def scrape(s):
     html = requests.get(s)
     soup = BeautifulSoup(html.text, "html.parser")
-    print(soup.find_all('tr'))
-    print(soup.get_text())
+    print(soup.prettify())
+    print(soup.title)
+
+    line = soup.find_all('tr')
+    # print(soup.get_text())
+    # for link in soup.find_all('a'):
+    #     print(link.get('href'))
+    # print(soup.get_text())
     with open('data.csv', 'w', newline='') as file:  # open csv file
         field_names = ['Rank', 'State', 'Code', 'Population']  # field name
         writer = csv.DictWriter(file, fieldnames=field_names)  # open a writer
         list = []
+        # for spot in line:
+        #     list.append(soup.find_all('td'))
         for link in soup.find_all('tr'):  # extract the url's and titles
             list.append({'Rank': link.get('td'), 'State': link.get('td'), 'Code': link.get('td'), 'Population': link.get('td')})
-        writer.writeheader()  # write the header
-        for row in list:  # write it over to the csv file
-            writer.writerow({'Rank': row['Rank'], 'State': row['State'], 'Code': row['Code'], 'Population': row['Population'],})
+        # print(list)
+        # writer.writeheader()  # write the header
+        # for row in list:  # write it over to the csv file
+        #     writer.writerow({'Rank': row['Rank'], 'State': row['State'], 'Code': row['Code'], 'Population': row['Population'],})
     file.close()
 
 if __name__ == "__main__":

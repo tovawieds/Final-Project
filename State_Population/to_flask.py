ab2eced5cf9dffe2c57069a4c8974d58d6def9ef
tovawieds <155400443+tovawieds@users.noreply.github.com>
2024-04-17 01:41:53 +0000

    07

--- State_Population/to_flask.py
+++ State_Population/to_flask.py
@@ -5,31 +5,68 @@ import os
 import sqlite3
 
 
+# set up web page
+HOST_NAME = "localhost"
+HOST_PORT = 80
+DBFILE = "db.sqlite"
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = sqlite3.connect('db.sqlite')
-    conn.row_factory = sqlite3.Row
-    states = conn.execute('SELECT * FROM State_Population').fetchall()
-    print(states)
-    print(conn)
-    return conn
+# get table from database
+def getusers():
+  conn = sqlite3.connect(DBFILE)
+  cursor = conn.cursor()
+  cursor.execute("SELECT * FROM `State_Population`")
+  results = cursor.fetchall()
+  conn.close()
+  return results
 
-
-def main():
-    get_db_connection()
-
-@app.route('/')
+# connect to database
+@app.route("/")
 def index():
-    conn = get_db_connection()
-    states = conn.execute('SELECT * FROM State_Population').fetchall()
+  users = getusers()
+  return render_template("base.html", usr=users)
+ 
+@app.post("/go")
+def numbers():
+    numbs = str(request.form.get('num'))
+    conn = sqlite3.connect(DBFILE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM `State_Population` WHERE `Population` >= ?", (numbs,))
+    results = cursor.fetchall()
     conn.close()
-    return render_template('index.html', states=states)
+    return render_template("base.html", rslt=results)
 
-
+# run
 if __name__ == "__main__":
-    main()
+  app.run(HOST_NAME, HOST_PORT)
 
+
+# ==========================================================================
+# app = Flask(__name__)
+
+# def get_db_connection():
+#     conn = sqlite3.connect('db.sqlite')
+#     conn.row_factory = sqlite3.Row
+#     states = conn.execute('SELECT * FROM State_Population').fetchall()
+#     print(states)
+#     print(conn)
+#     return conn
+
+
+# def main():
+#     get_db_connection()
+
+# @app.route('/')
+# def index():
+#     conn = get_db_connection()
+#     states = conn.execute('SELECT * FROM State_Population').fetchall()
+#     conn.close()
+#     return render_template('index.html', states=states)
+
+
+# if __name__ == "__main__":
+#     main()
+# ----------------------------------------------------------------
 # basedir = os.path.abspath(os.path.dirname(__file__))
 
 # app = Flask(__name__)
